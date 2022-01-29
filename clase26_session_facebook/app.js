89bbf52027fdd509ade801fee909bfe83439254e
Fabomarche <fabriciomarche@hotmail.com>
2022-01-29 19:11:54 -0300

    clase 26 v2

--- clase26_session_facebook/app.js
+++ clase26_session_facebook/app.js
@@ -6,7 +6,7 @@ import cookieParser from 'cookie-parser'
 import MongoStore from 'connect-mongo'
 import cors from 'cors'
 import { __dirname } from './utils.js'
-//import { engine } from 'express-handlebars'
+import { engine } from 'express-handlebars'
 import { Server } from 'socket.io'
 import ChatsService from './services/chatsService.js'
 import uploadService from './services/uploadService.js'
@@ -62,13 +62,13 @@ io.on('connection', socket => {
     })
 })
 
-//app.engine('handlebars', engine())
-//app.set('view engine', 'handlebars')
-app.set('views', './views')
+// app.engine('handlebars', engine())
+// app.set('view engine', 'handlebars')
+// app.set('views', './views')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
-app.use('/uploads/', express.static(__dirname + '/uploads'))
+// app.use('/uploads/', express.static(__dirname + '/uploads'))
 app.use(express.static(__dirname + '/public'))
 
 app.use(session({
@@ -86,83 +86,91 @@ initializePassportConfig()
 app.use(passport.initialize())
 app.use(passport.session())
 //Rutas Facebook
-app.get('/auth/facebook', passport.authenticate('facebook',{scope:['email']},(req,res)=>{
-    
-}))
-app.get('auth/facebook/callback', passport.authenticate('facebook', {
+app.get('/auth/facebook',passport.authenticate('facebook',{scope:['email']}),(req,res)=>{
+
+})
+app.get('/auth/facebook/callback',passport.authenticate('facebook',{
     failureRedirect:'/paginadeFail'
 }),(req,res)=>{
-    res.send({message:"Logueado!"})
+    res.send({message:"FINALMENTE, logueado :)"})
 })
+
+app.get('/fail', (req, res) => {
+    res.send({ status: 'error', message: 'Ha fallado el inicio de sesión en Facebook.' })
+  })
+  
+  app.get('/logout', (req, res) => {
+    req.logout()
+  })
 
 //Rutas
-app.use('/api/chats', chats)
+// app.use('/api/chats', chats)
 
-app.post('/api/register', uploadService.single('avatar'), async (req, res) => {
-    try {
-        const file = req.file
-        const user = req.body
-        user.age = parseInt(user.age)
-        user.avatar = `${req.protocol}://${req.hostname}:${PORT}/uploads/${file.filename}`
+// app.post('/api/register', uploadService.single('avatar'), async (req, res) => {
+//     try {
+//         const file = req.file
+//         const user = req.body
+//         user.age = parseInt(user.age)
+//         user.avatar = `${req.protocol}://${req.hostname}:${PORT}/uploads/${file.filename}`
     
-        const emailFound = await UserModel.findOne({ email: user.email })
-        if (emailFound) throw new Error('Email already exist.')
+//         const emailFound = await UserModel.findOne({ email: user.email })
+//         if (emailFound) throw new Error('Email already exist.')
     
-        const usernameFound = await UserModel.findOne({ username: user.username })
-        if (usernameFound) throw new Error('Username already exist.')
+//         const usernameFound = await UserModel.findOne({ username: user.username })
+//         if (usernameFound) throw new Error('Username already exist.')
     
-        await UserModel.create(user)
-        res.send({ status: 'success', message: 'Registration has been successfully.' })
-    } catch (err) {
-    console.error(err)
-    res.status(400).send({ status: 'error', message: err.message })
-    }
-})
+//         await UserModel.create(user)
+//         res.send({ status: 'success', message: 'Registration has been successfully.' })
+//     } catch (err) {
+//     console.error(err)
+//     res.status(400).send({ status: 'error', message: err.message })
+//     }
+// })
 
-app.post('/api/login', async (req, res) => {
-    try {
-        const { email, password } = req.body
-        if (!email || !password) throw new Error('Username or password are incorrect.')
-        const user = await UserModel.findOne({ email: email })
-        if (!user) throw new Error('User not found.')
-        if (user.password !== password) throw new Error('Incorrect password.')
-        req.session.user = {
-        username: user.username,
-        email: user.email
-        }
-        res.send({ status: 'success' })
-    } catch (err) {
-        console.error(err)
-        res.status(400).send({ status: 'error', message: err.message })
-    }
-})
+// app.post('/api/login', async (req, res) => {
+//     try {
+//         const { email, password } = req.body
+//         if (!email || !password) throw new Error('Username or password are incorrect.')
+//         const user = await UserModel.findOne({ email: email })
+//         if (!user) throw new Error('User not found.')
+//         if (user.password !== password) throw new Error('Incorrect password.')
+//         req.session.user = {
+//         username: user.username,
+//         email: user.email
+//         }
+//         res.send({ status: 'success' })
+//     } catch (err) {
+//         console.error(err)
+//         res.status(400).send({ status: 'error', message: err.message })
+//     }
+// })
 
-app.get('/api/login', (req, res) => {
-    if (req.session.user)
-        res.send(req.session.user)
-    else
-        res.send({ status: 'error', message: 'You are not log in.' })
-})
+// app.get('/api/login', (req, res) => {
+//     if (req.session.user)
+//         res.send(req.session.user)
+//     else
+//         res.send({ status: 'error', message: 'You are not log in.' })
+// })
 
-app.post('/api/logout', (req, res) => {
-    const { username } = req.session.user
-    req.session.user = null
-    res.send({ status: 'success', payload: { username: username } })
-})
+// app.post('/api/logout', (req, res) => {
+//     const { username } = req.session.user
+//     req.session.user = null
+//     res.send({ status: 'success', payload: { username: username } })
+// })
 
 //Render Views
-app.get('/', (req, res) => {
-    res.render('login')
-})
+// app.get('/', (req, res) => {
+//     res.render('login')
+// })
 
-app.get('/register', (req, res) => {
-    res.render('register')
-})
+// app.get('/register', (req, res) => {
+//     res.render('register')
+// })
 
-app.get('/chat', (req, res) => {
-    res.render('chat')
-})
+// app.get('/chat', (req, res) => {
+//     res.render('chat')
+// })
 
-app.get('/logout', (req, res) => {
-    res.render('logout')
-})
+// app.get('/logout', (req, res) => {
+//     res.render('logout')
+// })
